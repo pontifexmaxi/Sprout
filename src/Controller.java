1a135dfda557f77ec0b5f1ca14371e1a5ea76a52
Markus <pontifex@live.dk>
2018-02-05 17:26:51 +0100

    Oldschool

--- src/Controller.java
+++ src/Controller.java
@@ -2,5 +2,32 @@ import edu.princeton.cs.algs4.*;
 
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        StdOut.println(" .d8888b.                                    888    \n" +
+                "d88P  Y88b                                   888    \n" +
+                "Y88b.                                        888    \n" +
+                " \"Y888b.   88888b.  888d888 .d88b.  888  888 888888 \n" +
+                "    \"Y88b. 888 \"88b 888P\"  d88\"\"88b 888  888 888    \n" +
+                "      \"888 888  888 888    888  888 888  888 888    \n" +
+                "Y88b  d88P 888 d88P 888    Y88..88P Y88b 888 Y88b.  \n" +
+                " \"Y8888P\"  88888P\"  888     \"Y88P\"   \"Y88888  \"Y888 \n" +
+                "           888                                      \n" +
+                "           888                                      \n" +
+                "           888                                      \n" +
+                "\n");
+        StdOut.println("WELCOME TO SPROUT THE GEDCOM CREATOR!");
+        StdOut.println("FIRST NAME");
+        String firstName = StdIn.readString();
+        StdOut.println("MIDDLE NAME");
+        String middleName = StdIn.readString();
+        StdOut.println("LAST NAME");
+        String lastName = StdIn.readString();
+        StdOut.println("DAY OF BIRTH");
+        byte birthDay = StdIn.readByte();
+        StdOut.println("MONTH OF BIRTH");
+        byte birthMonth = StdIn.readByte();
+        StdOut.println("YEAR OF BIRTH");
+        short birthYear = StdIn.readShort();
+        Person person = new Person(firstName, middleName, lastName, birthDay, birthMonth, birthYear);
+        StdOut.println(person.firstName + " " + person.middleName + " " + person.lastName + " " + person.birthDay + " " + person.birthMonth + " " + person.birthYear);
     }
+}
--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,40 @@
 public class Person {
-    private String prefix, suffix,
-            firstName, middleName, lastName,
-            birthPlaceCity, birthPlaceCountry, christeningPlaceCity, christeningPlaceCountry,
-            deathPlaceCity, deathPlaceCountry, burialPlaceCity, burialPlaceCountry;
-    private short birthDay, birthMonth, birthYear,
-            christeningDay, christeningMonth, christeningYear,
-            deathDay, deathMonth, deathYear,
-            burialDay, burialMonth, burialYear;
+    private String prefix;
+    private String suffix;
+    String firstName;
+    String middleName;
+    String lastName;
+    private String birthPlaceCity;
+    private String birthPlaceCountry;
+    private String christeningPlaceCity;
+    private String christeningPlaceCountry;
+    private String deathPlaceCity;
+    private String deathPlaceCountry;
+    private String burialPlaceCity;
+    private String burialPlaceCountry;
+    short birthYear;
+    private short christeningYear;
+    private short deathYear;
+    private short burialYear;
     private Person mom, dad;
     private Person[] children, siblings;
     private boolean isAlive;
     private byte gender;
+    byte birthDay;
+    byte birthMonth;
+    private byte christeningDay;
+    private byte christeningMonth;
+    private byte deathDay;
+    private byte deathMonth;
+    private byte burialDay;
+    private byte burialMonth;
+
+    public Person(String firstName, String middleName, String lastName, byte birthDay, byte birthMonth, short birthYear) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+    }
 }
