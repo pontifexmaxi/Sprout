9de82412a323a67717d5f33f7b7f41713d5b233b
Markus <pontifex@live.dk>
2018-02-05 16:28:36 +0100

    Relationships

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,6 @@
+import edu.princeton.cs.algs4.*;
+
 public class Controller {
-}
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
---
+++ src/Relationship.java
@@ -0,0 +1,6 @@
+public class Relationship {
+    private Person spouse1, spouse2;
+    private short day, month, year;
+    private String placeCity, placeCountry;
+    private String[] type = {"Marriage", "Partnership"};
+}
