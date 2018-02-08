218c1ee19196f329cc27b2cc7b26cd14bf65ed3a
Markus <pontifex@live.dk>
2018-02-08 12:32:11 +0100

    Parser fremskridt

--- src/Controller.java
+++ src/Controller.java
@@ -12,8 +12,8 @@ public class Controller {
                 " \"Y8888P\"  88888P\"  888     \"Y88P\"   \"Y88888  \"Y888 \n" +
                 "           888                                      \n" +
                 "           888                                      \n" +
-                "           888                                      \n" +
-                "\n");
+                "           888                                      \n");
+        /**
         StdOut.println("WELCOME TO SPROUT THE GEDCOM CREATOR!");
         StdOut.println("FIRST NAME");
         String firstName = StdIn.readString();
@@ -29,5 +29,68 @@ public class Controller {
         short birthYear = StdIn.readShort();
         Person person = new Person(firstName, middleName, lastName, birthDay, birthMonth, birthYear);
         StdOut.println(person.firstName + " " + person.middleName + " " + person.lastName + " " + person.birthDay + " " + person.birthMonth + " " + person.birthYear);
+        **/
+
+        byte birthDay = 1;
+        String birthMonth = "JAN";
+        short birthYear = 2018;
+        Person person = new Person("Given Name", "Last Name", birthDay, birthMonth, birthYear);
+
+        //StdIn.readChar();
+        for (int i = 0; i < 18; i++) {
+            StdIn.readLine();
+        }
+        while (StdIn.hasNextLine()) {
+            byte num = StdIn.readByte();
+            StdOut.println(num);
+            StdIn.readChar();
+            String type = StdIn.readString();
+
+            switch (num) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                switch (type) {
+                    case "NAME":
+                        StdIn.readLine();
+                        break;
+                    case "GIVN":
+                        String givenName = StdIn.readLine();
+                        person.setGivenName(givenName);
+                        break;
+                    case "SURN":
+                        String lastName = StdIn.readString();
+                        person.setLastName(lastName);
+                        break;
+                    case "SEX":
+                        StdIn.readChar();
+                        char gender = StdIn.readChar();
+                        person.setGender(gender);
+                        break;
+                    case "BIRT":
+                        break;
+                    case "DATE":
+                        birthDay = StdIn.readByte();
+                        person.setBirthDay(birthDay);
+                        birthMonth = StdIn.readString();
+                        person.setBirthMonth(birthMonth);
+                        birthYear = StdIn.readShort();
+                        person.setBirthYear(birthYear);
+                        StdOut.println(person.getGivenName() + person.getLastName() + person.getGender() + person.getBirthDay() + person.getBirthMonth() + person.getBirthYear());
+                        break;
+                    case "PLAC":
+                        StdIn.readLine();
+                    case "BAPM":
+                        StdIn.readLine();
+                    default:
+                        person = new Person("Given Name", "Last Name", birthDay, birthMonth, birthYear);
+                        StdOut.println("ups");
+                        break;
+                }
+            }
+
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,8 @@
 public class Person {
     private String prefix;
     private String suffix;
-    String firstName;
-    String middleName;
-    String lastName;
+    private String givenName;
+    private String lastName;
     private String birthPlaceCity;
     private String birthPlaceCountry;
     private String christeningPlaceCity;
@@ -12,7 +11,7 @@ public class Person {
     private String deathPlaceCountry;
     private String burialPlaceCity;
     private String burialPlaceCountry;
-    short birthYear;
+    private short birthYear;
     private short christeningYear;
     private short deathYear;
     private short burialYear;
@@ -20,8 +19,8 @@ public class Person {
     private Person[] children, siblings;
     private boolean isAlive;
     private char gender;
-    byte birthDay;
-    byte birthMonth;
+    private byte birthDay;
+    private String birthMonth;
     private byte christeningDay;
     private byte christeningMonth;
     private byte deathDay;
@@ -29,12 +28,251 @@ public class Person {
     private byte burialDay;
     private byte burialMonth;
 
-    public Person(String firstName, String middleName, String lastName, byte birthDay, byte birthMonth, short birthYear) {
-        this.firstName = firstName;
-        this.middleName = middleName;
+    public Person(String givenName, String lastName, byte birthDay, String birthMonth, short birthYear) {
+        this.givenName = givenName;
         this.lastName = lastName;
         this.birthDay = birthDay;
         this.birthMonth = birthMonth;
         this.birthYear = birthYear;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthPlaceCity() {
+        return birthPlaceCity;
+    }
+
+    public void setBirthPlaceCity(String birthPlaceCity) {
+        this.birthPlaceCity = birthPlaceCity;
+    }
+
+    public String getBirthPlaceCountry() {
+        return birthPlaceCountry;
+    }
+
+    public void setBirthPlaceCountry(String birthPlaceCountry) {
+        this.birthPlaceCountry = birthPlaceCountry;
+    }
+
+    public String getChristeningPlaceCity() {
+        return christeningPlaceCity;
+    }
+
+    public void setChristeningPlaceCity(String christeningPlaceCity) {
+        this.christeningPlaceCity = christeningPlaceCity;
+    }
+
+    public String getChristeningPlaceCountry() {
+        return christeningPlaceCountry;
+    }
+
+    public void setChristeningPlaceCountry(String christeningPlaceCountry) {
+        this.christeningPlaceCountry = christeningPlaceCountry;
+    }
+
+    public String getDeathPlaceCity() {
+        return deathPlaceCity;
+    }
+
+    public void setDeathPlaceCity(String deathPlaceCity) {
+        this.deathPlaceCity = deathPlaceCity;
+    }
+
+    public String getDeathPlaceCountry() {
+        return deathPlaceCountry;
+    }
+
+    public void setDeathPlaceCountry(String deathPlaceCountry) {
+        this.deathPlaceCountry = deathPlaceCountry;
+    }
+
+    public String getBurialPlaceCity() {
+        return burialPlaceCity;
+    }
+
+    public void setBurialPlaceCity(String burialPlaceCity) {
+        this.burialPlaceCity = burialPlaceCity;
+    }
+
+    public String getBurialPlaceCountry() {
+        return burialPlaceCountry;
+    }
+
+    public void setBurialPlaceCountry(String burialPlaceCountry) {
+        this.burialPlaceCountry = burialPlaceCountry;
+    }
+
+    public short getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(short birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public short getChristeningYear() {
+        return christeningYear;
+    }
+
+    public void setChristeningYear(short christeningYear) {
+        this.christeningYear = christeningYear;
+    }
+
+    public short getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(short deathYear) {
+        this.deathYear = deathYear;
+    }
+
+    public short getBurialYear() {
+        return burialYear;
+    }
+
+    public void setBurialYear(short burialYear) {
+        this.burialYear = burialYear;
+    }
+
+    public Person getMom() {
+        return mom;
+    }
+
+    public void setMom(Person mom) {
+        this.mom = mom;
+    }
+
+    public Person getDad() {
+        return dad;
+    }
+
+    public void setDad(Person dad) {
+        this.dad = dad;
+    }
+
+    public Person[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Person[] children) {
+        this.children = children;
+    }
+
+    public Person[] getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(Person[] siblings) {
+        this.siblings = siblings;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public byte getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(byte birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(String birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public byte getChristeningDay() {
+        return christeningDay;
+    }
+
+    public void setChristeningDay(byte christeningDay) {
+        this.christeningDay = christeningDay;
+    }
+
+    public byte getChristeningMonth() {
+        return christeningMonth;
+    }
+
+    public void setChristeningMonth(byte christeningMonth) {
+        this.christeningMonth = christeningMonth;
+    }
+
+    public byte getDeathDay() {
+        return deathDay;
+    }
+
+    public void setDeathDay(byte deathDay) {
+        this.deathDay = deathDay;
+    }
+
+    public byte getDeathMonth() {
+        return deathMonth;
+    }
+
+    public void setDeathMonth(byte deathMonth) {
+        this.deathMonth = deathMonth;
+    }
+
+    public byte getBurialDay() {
+        return burialDay;
+    }
+
+    public void setBurialDay(byte burialDay) {
+        this.burialDay = burialDay;
+    }
+
+    public byte getBurialMonth() {
+        return burialMonth;
+    }
+
+    public void setBurialMonth(byte burialMonth) {
+        this.burialMonth = burialMonth;
     }
 }
